89c906feb244df9ecd0b3168cb0ac0e7770d28e3
Bakhara Alief <ara.bakhara12@gmail.com>
2024-11-26 10:37:15 +0700

    update gradle

--- core-ui/build.gradle.kts
+++ core-ui/build.gradle.kts
@@ -27,14 +27,11 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
     buildFeatures {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
+        kotlinCompilerExtensionVersion = libs.versions.composeCompilerExtension.get()
     }
     kotlinOptions {
         jvmTarget = "1.8"
--- feature/home/build.gradle.kts
+++ feature/home/build.gradle.kts
@@ -33,7 +33,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
+        kotlinCompilerExtensionVersion = libs.versions.composeCompilerExtension.get()
     }
     kotlinOptions {
         jvmTarget = "1.8"
